e7a467e9492cd765ff3c4b6449812ef5c3bb58ca
JeAmoTu <JeAmoTu@gmail.com>
2017-01-19 23:44:32 +0800

    Playing Interface

---
+++ Card/Assets/Scripts/Hero1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero1 : MonoBehaviour {
+
+    private UISprite sprite;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Card/Assets/Scripts/Hero2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero2 : MonoBehaviour {
+    private UISprite sprite;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
