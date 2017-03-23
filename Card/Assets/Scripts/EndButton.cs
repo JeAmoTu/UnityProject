23e03853405d16fc990b16fc9cc5117d678150ba
JeAmoTu <JeAmoTu@gmail.com>
2017-03-23 22:22:34 +0800

    Modify End Button

---
+++ Card/Assets/Scripts/EndButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndButton : MonoBehaviour
+{
+    private UILabel mLabel;
+
+    private void Awake()
+    {
+        mLabel = transform.Find("Label").GetComponent<UILabel>();
+    }
+    public void onButtonClick()
+    {
+        mLabel.text = "对方回合";
+    }
+}
